f221f50c8197f8e0dd6d3270a07086dcb6ceb65a
python_test <00whdrn00@naver.com>
2019-11-28 17:03:50 +0900

    2019.11.28 Data 검색 관련 Python Commit

---
+++ ip_search.py
@@ -0,0 +1,32 @@
+# -*- coding: cp949 -*-
+import sys
+import os.path
+import re
+
+# path
+def abs_path():
+    path = os.path.dirname(os.path.abspath(__file__))
+    return (path)    
+
+def ip_result(ip_list):
+    with open("ip_list.log", "w") as f2:
+        for result in ip_list:
+            f2.write("%s\n" % result)
+
+if __name__=="__main__":
+    # IP regex
+    ip_regex = re.compile(r'\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b')
+
+# file_name = search file name
+with open("file_name", "r") as f1:
+    text = f1.read()
+
+    ip_list = ip_regex.findall(text)
+    ip_list = list(set(ip_list))
+    ip_list.remove('255.255.255.0')
+    ip_list.remove('0.0.0.0')
+    ip_list.sort()
+    print("Searching . . .")
+    ip_result(ip_list)
+    print("End")
+
