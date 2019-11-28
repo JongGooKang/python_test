031b210e40009e18de950d169ebbe1311ef31858
python_test <00whdrn00@naver.com>
2019-11-28 17:33:47 +0900

    2019.11.28 ip_search.pu Update Commit

--- ip_search.py
+++ ip_search.py
@@ -1,7 +1,10 @@
 # -*- coding: cp949 -*-
 import sys
+import os
 import os.path
 import re
+
+file_path = raw_input('File_Path : ')
 
 # path
 def abs_path():
@@ -17,8 +20,8 @@ if __name__=="__main__":
     # IP regex
     ip_regex = re.compile(r'\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b')
 
-# file_name = search file name
-with open("file_name", "r") as f1:
+# file_path = search file name
+with open(file_path, "r") as f1:
     text = f1.read()
 
     ip_list = ip_regex.findall(text)
@@ -29,4 +32,3 @@ with open("file_name", "r") as f1:
     print("Searching . . .")
     ip_result(ip_list)
     print("End")
-
